6ecb62d1147fdf30b3aa59ac7374816b60151f97
werdnaknom <andrew.m.monk@gmail.com>
2022-08-07 15:23:57 -0700

    Added path to ErrorEntity

--- Entities/error_entity.py
+++ Entities/error_entity.py
@@ -8,6 +8,7 @@ class ErrorEntity:
     _id = None
     error_traceback: str
     error_msg: str
+    path: str = None
     modified_date: datetime.datetime = field(
         default=datetime.datetime.utcnow(), repr=False, compare=False)
     created_date: datetime.datetime = field(
@@ -20,3 +21,7 @@ class ErrorEntity:
 
     def to_mongo(self) -> dict:
         return asdict(self)
+
+    @classmethod
+    def from_mongo(cls, adict):
+        return cls(**adict)
