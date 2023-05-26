ade09a12442868d669d43d3cb28abab8304b29ab
werdnaknom <andrew.m.monk@gmail.com>
2023-05-26 14:42:56 -0700

    Adding request objects to Entities class

---
+++ RequestResponse/WebappProcessingRequest/webapp_processing_request.py
@@ -0,0 +1,22 @@
+import typing as t
+import collections
+
+from ..requests import RequestObject, ValidRequestObject, InvalidRequestObject
+
+
+class WebAppProcessingRequest(ValidRequestObject):
+
+    def __init__(self, filters=None):
+        self.filters = filters
+
+    @classmethod
+    def from_dict(cls, adict):
+        invalid_req = InvalidRequestObject()
+
+        if 'filters' in adict and not isinstance(adict['filters'], collections.Mapping):
+            invalid_req.add_error('filters', 'Is not iterable')
+
+        if invalid_req.has_errors():
+            return invalid_req
+
+        return WebAppProcessingRequest(filters=adict.get('filters', None))
